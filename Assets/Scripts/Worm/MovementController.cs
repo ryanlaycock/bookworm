d4194c11823390bfdf8f74a104eaffcfdc1a5e13
ryanlaycock <ryan.laycock@hotmail.co.uk>
2023-09-16 02:32:00 +0100

    Double jumping

--- Assets/Scripts/Worm/MovementController.cs
+++ Assets/Scripts/Worm/MovementController.cs
@@ -6,24 +6,20 @@ using UnityEngine.UIElements;
 public class MovementController : MonoBehaviour
 {
     private float _moveSpeed = 7;
-    private float _jumpSpeed = 6;
+    private float _jumpSpeed = 7;
     public bool _canMove = true;
     private Rigidbody2D _rb;
     private bool _canJump = false;
+    private int _jumpsRemaining = 2;
 
     void Start() {
         _rb = GetComponent<Rigidbody2D>();
     }
-    
-    void FixedUpdate()
-    {
-        MoveHorizontal();
-        Jump();
-    }
 
     void Update()
     {
-
+        MoveHorizontal();
+        Jump();
     }
 
     private void MoveHorizontal()
@@ -35,22 +31,26 @@ public class MovementController : MonoBehaviour
 
     private void Jump()
     {
-        float input = Input.GetAxis("Vertical");
-        if (_canJump  && input > 0)
+        bool input = Input.GetKeyDown(KeyCode.W);
+        if ((_canJump || _jumpsRemaining > 0) && input)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpSpeed);
             _canJump = false;
+            _jumpsRemaining --;
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        _canJump = collision.gameObject.CompareTag("Ground");
+        // TODO Check if top of box is > bottom of player (stuck to side)
+        
+        _canJump = collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Book");
+        _jumpsRemaining = 2;
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        _canJump = false;  
+        _canJump = false;
     }
     
 }
